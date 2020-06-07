fa5cbd281bf15cb4b37f7dcaf596ebe8ab1d5885
colin-xiao <648438974@qq.com>
2020-06-07 22:22:08 +0800

    detail-banner finish

---
+++ src/pages/detail/components/Banner.vue
@@ -0,0 +1,51 @@
+<template>
+  <div class="banner">
+    <img  class="banner-img" src="http://img1.qunarzz.com/sight/p0/2005/a9/a9411416ff215fe2a3.water.jpg_600x330_feb1c240.jpg" alt="">
+    <div class="banner-info">
+      <div class="banner-title">上海迪士尼乐园</div>
+      <div class="banner-number"><span class="iconfont banner-icon">&#xe635;104</span>
+</div>
+    </div>
+  </div>
+</template>
+<script>
+export default{
+  name:"DetailBanner"
+}
+</script>
+<style scoped lang='stylus'>
+  .banner
+    position relative
+    overflow hidden
+    height 0
+    padding-bottom 55%
+    color #fff
+    .banner-img
+      width 100%
+    .banner-info
+      display flex
+      position absolute
+      left 0
+      right 0
+      bottom 0
+      line-height .6rem
+      background-image liner-gradient(top,rgba(0,0,0,0),rgba(0,0,0,0.8))
+      .banner-title
+        flex 1
+        font-size .4rem
+        padding 0 .2rem
+      .banner-number
+        margin-top .2rem
+        padding 0 .4rem
+        line-height .32rem
+        height .32rem
+        border-radius .2rem
+        background rgba(0,0,0,.8) 
+        font-size .24rem
+        .banner-icon
+          font-size .24rem
+
+
+
+
+</style>
--- src/pages/home/components/Recommend.vue
+++ src/pages/home/components/Recommend.vue
@@ -2,14 +2,14 @@
   <div>
     <div class="title">热销推荐</div>
     <ul>
-      <li class="item border-bottom" v-for="item of list" :key="item.index">
-        <img class="item-img" :src="item.imgUrl" alt="">
-        <div class="item-info">
-          <p class="item-title" id="item-title">{{item.title}}</p>
-          <p class="item-desc">{{item.price}}</p>
-          <button class="item-button" id="item-button">查看详情</button>
-        </div>
-      </li>
+        <router-link tag="li" class="item border-bottom" v-for="item of list" :key="item.id" :to="'/detail/'+item.id">
+          <img class="item-img" :src="item.imgUrl" alt="">
+          <div class="item-info">
+            <p class="item-title" id="item-title">{{item.title}}</p>
+            <p class="item-desc">{{item.price}}</p>
+            <button class="item-button" id="item-button">查看详情</button>
+          </div>
+        </router-link>
 
     </ul>
   </div>
