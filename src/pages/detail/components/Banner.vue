29276a65d488817252c17bf19682303fb0482ced
colin-xiao <648438974@qq.com>
2020-06-08 14:25:01 +0800

    banner finish

--- src/pages/detail/components/Banner.vue
+++ src/pages/detail/components/Banner.vue
@@ -1,16 +1,42 @@
 <template>
-  <div class="banner">
-    <img  class="banner-img" src="http://img1.qunarzz.com/sight/p0/2005/a9/a9411416ff215fe2a3.water.jpg_600x330_feb1c240.jpg" alt="">
-    <div class="banner-info">
-      <div class="banner-title">上海迪士尼乐园</div>
-      <div class="banner-number"><span class="iconfont banner-icon">&#xe635;104</span>
-</div>
+  <div>
+    <div class="banner" @click="handleBannerClick">
+      <img  class="banner-img" src="http://img1.qunarzz.com/sight/p0/2005/a9/a9411416ff215fe2a3.water.jpg_600x330_feb1c240.jpg" alt="">
+      <div class="banner-info">
+        <div class="banner-title">上海迪士尼乐园</div>
+        <div class="banner-number"><span class="iconfont banner-icon">&#xe635;104</span></div>
+      </div>
     </div>
+    <common-gallary 
+    :imgs = "imgs" 
+    v-show="showGallary" 
+    @close = "handleGallaryClose"
+    />
   </div>
 </template>
 <script>
+import CommonGallary from 'common/gallary/Gallary';
+
 export default{
-  name:"DetailBanner"
+  name:"DetailBanner",
+  components: {
+    CommonGallary
+  },
+  data () {
+    return {
+      imgs:['http://img1.qunarzz.com/sight/p0/1905/3f/3f76721b9a43d89ba3.img.jpg_r_800x800_1fa239fd.jpg','http://img1.qunarzz.com/sight/p0/1906/e9/e9b903f81b992acca3.img.jpg_r_800x800_84713129.jpg'],
+      showGallary:false
+    }
+  },
+  methods: {
+    handleBannerClick(){
+      this.showGallary = true
+    },
+    handleGallaryClose(){
+      this.showGallary = false
+    }
+    
+  }
 }
 </script>
 <style scoped lang='stylus'>
@@ -29,7 +55,7 @@ export default{
       right 0
       bottom 0
       line-height .6rem
-      background-image liner-gradient(top,rgba(0,0,0,0),rgba(0,0,0,0.8))
+      background-imagec liner-gradient(top,rgba(0,0,0,0),rgba(0,0,0,0.8))
       .banner-title
         flex 1
         font-size .4rem
