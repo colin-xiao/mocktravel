6bc302239a98910c6fe59a9d92fd63778991dd79
colin-xiao <648438974@qq.com>
2020-07-04 21:52:04 +0800

    fix-problem

--- src/pages/detail/components/Header.vue
+++ src/pages/detail/components/Header.vue
@@ -30,6 +30,9 @@ export default{
       
     window.addEventListener('scroll', this.handleScroll)
   },
+  deactivated () {
+    window.removeEventListener('scroll', this.handleScroll)
+  },
     methods: {
     handleScroll () {
       const top = document.documentElement.scrollTop
